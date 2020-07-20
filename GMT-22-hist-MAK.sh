d05b470505b1c31b0928efa44a5d1e1d9415a13d
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-07-20 20:49:30 +0300

    Add files via upload

--- GMT-22-hist-MAK.sh
+++ GMT-22-hist-MAK.sh
@@ -9,38 +9,47 @@ gmt set MAP_TITLE_OFFSET 0.5c \
     FONT_TITLE 10p,Palatino-Roman,black \
     FONT_ANNOT_PRIMARY 9p,Palatino-Roman,dimgray \
     FONT_LABEL 8p,Palatino-Roman,dimgray \
-# Step-2. Generate a file
+
+# Generate a file
 ps=Hist_MAK.ps
-# Step-3. Plot rose diagram
+
+# Plot rose diagram
 gmt psrose tableMAK.txt -i1,4 -R0/1/0/360 \
     -A7r -S1.0in \
     -Glightsteelblue1 -Bx0.2g0.2 \
     --MAP_TICK_PEN_PRIMARY=thinner,dimgray \
     --MAP_GRID_PEN=thinnest,dimgray \
+    --FONT_ANNOT_PRIMARY=9p,Helvetica,black \
+    --FONT_LABEL=9p,Helvetica,black \
     -By30g30+l"Bathymetric data distribution by cross-section profiles" \
     -B+t"Rose diagram on the cross-section profiles"+gghostwhite \
     -M0.5c+e+gred2+n1c \
     -W0.1p,red -Cm \
     -UBL/-15p/-65p -Vv -K > $ps
-# Step-4. Plot histogram
-gmt pshistogram tableMAK.txt -i4 -R-4000/900/0/27 -JX4.8i/2.4i -X3.6i \
+
+# Plot histogram
+gmt pshistogram tableMAK.txt -i4 -R-4000/900/0/25 -JX4.8i/2.4i -X3.6i \
     -Bpxg1000a1000f100+l"Bathymetry (m)" \
     -Bpyg5a5f2.5+l"Frequency distribution"+u" %" -Bsyg2.5 \
     --MAP_TITLE_OFFSET=0.5c \
     --MAP_TICK_PEN_PRIMARY=thinner,dimgray \
     --MAP_GRID_PEN=thinnest,dimgray \
+    --FONT_ANNOT_PRIMARY=9p,Helvetica,black \
+    --FONT_LABEL=9p,Helvetica,black \
     -BWSne+t"Makran Trench: Histograms of the depths values on the cross-section profiles"+gghostwhite -Gpowderblue \
-    -D+f6p,Times-Roman -L0.1p,dimgray -Z1 -W250 -N0+pred -N1+pblue -N2+pgreen \
+    -D+f8p,Helvetica -L0.1p,dimgray -Z1 -W250 -N0+pred -N1+pblue -N2+pgreen \
     -O -K >> $ps
-# Step-5. Add legend
+
+# Add legend
 gmt pslegend -R -J -Dx1.0/-2.6+w5.0c+o-1.0/0.5c \
     -F+pthick+ithinner+gwhite \
-    --FONT_ANNOT_PRIMARY=8p -O -K << EOF >> $ps
+    --FONT_ANNOT_PRIMARY=8p,Helvetica -O -K << EOF >> $ps
 S 0.3c - 0.7c - 0.5p,red 1.0c Mean and standard deviation
 S 0.3c - 0.7c - 0.5p,green 1.0c LMS mode and scale
 S 0.3c - 0.7c - 0.5p,blue 1.0c Median and L1 scale
 EOF
-# Step-6. Add GMT logo
+
+# Add GMT logo
 gmt logo -Dx6.0/0.0+o-0.5c/-2.5c+w2c -O >> $ps
-# Step-7. Convert to image file using GhostScript (portrait orientation, 720 dpi)
+# Convert to image file using GhostScript (portrait orientation, 720 dpi)
 gmt psconvert Hist_MAK.ps -A0.2c -E720 -P -Tj -Z
